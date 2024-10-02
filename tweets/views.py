6a2da6eccc49ab6a9cac78e91cea1ebcf04e1601
znu <znu4847@gmail.com>
2024-10-02 21:08:51 +0900

    add '/' page

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Tweet
+
 
 # Create your views here.
+def see_all_tweets(request):
+    tweets = Tweet.objects.all()
+
+    return render(
+        request,
+        "tweets.html",
+        {"tweets": tweets},
+    )
